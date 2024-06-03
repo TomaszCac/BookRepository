41c814b4cc96bef0da5bc05dae7327a5c3180b31
Tomasz Cac <tomaszandrzej1234@gmail.com>
2024-06-03 17:55:11 +0200

    Added book model

--- BookRepository/Models/Book.cs
+++ BookRepository/Models/Book.cs
@@ -1,6 +1,31 @@
-﻿namespace BookRepository.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookRepository.Models
 {
     public class Book
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Author { get; set; }
+
+        [Required]
+        [Range(1000, 9999)]
+        public int Year { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public int Pages { get; set; }
     }
 }
