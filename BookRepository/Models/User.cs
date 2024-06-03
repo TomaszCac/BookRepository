fb490aa11f9d133ad5102f1918b80aeb715dfbf4
Tomasz Cac <tomaszandrzej1234@gmail.com>
2024-06-03 18:04:22 +0200

    Added user model

---
+++ BookRepository/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookRepository.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+        
+    }
+}
