6bedf9af8c4091d0c70e54eddaa118de76432304
Tomasz Cac <tomaszandrzej1234@gmail.com>
2024-06-03 18:16:03 +0200

    Added review model

---
+++ BookRepository/Models/Review.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookRepository.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int BookId {  get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Content { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public int Rating { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+    }
+}
