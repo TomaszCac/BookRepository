ef2447b53b9b8ae9030cfc09453d27215fda346c
Tomasz Cac <tomaszandrzej1234@gmail.com>
2024-06-06 17:26:45 +0200

    Added Entity Framework

---
+++ BookRepository/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using BookRepository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRepository.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
